b8e1b4bff9dbc181f104e6e1a84a9a23817437b5
soujiro1217 <134843711+soujiro1217@users.noreply.github.com>
2023-06-16 16:40:44 +0000

    [add]3-15保存機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
-  def book
+  def books
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
-  get '/book' => 'books#book'
+  post 'books' => 'books#create'
+  get '/books' => 'books#books'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
