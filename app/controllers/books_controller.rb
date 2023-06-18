99a3afabe0a6bc5729cbdb7028a213712ef6d2f8
soujiro1217 <134843711+soujiro1217@users.noreply.github.com>
2023-06-18 12:51:01 +0000

    [Update] 3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,50 @@
 class BooksController < ApplicationController
-  def books
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-private
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
